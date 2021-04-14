4c37e6d137c289b96fa1888b9f02c06476696695
filippofinke <filippofinke@protonmail.ch>
2021-04-14 20:49:52 +0200

    Added meta tags

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { useTranslation } from "react-i18next";
+import Container from "./components/Container";
+import Section from "./components/Section";
 
 const App = () => {
   const { t } = useTranslation();
 
-  return <h1>{t("hello")}</h1>;
+  return (
+    <Container>
+      <Section>
+        <h1>{t("hello")}</h1>
+      </Section>
+    </Container>
+  );
 };
 
 export default App;
---
+++ src/components/Section.js
@@ -0,0 +1,5 @@
+const Section = ({ children }) => {
+  return <section>{children}</section>;
+};
+
+export default Section;
