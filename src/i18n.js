c339fdbb2d4dddc1b804b1a0dee4a431bb0f43f5
filippofinke <filippofinke@protonmail.ch>
2021-04-15 13:37:08 +0200

    Fixed fallback languages

--- src/i18n.js
+++ src/i18n.js
@@ -3,26 +3,14 @@ import { initReactI18next } from "react-i18next";
 
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
-// don't want to use this?
-// have a look at the Quick start guide
-// for passing in lng and translations on init
-
 i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-  // learn more: https://github.com/i18next/i18next-http-backend
   .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    locales: ["en", "de", "fr", "it"],
-    defaultLocale: "en",
-    fallbackLng: "en",
-    debug: true,
+    supportedLngs: ["en", "de", "fr", "it"],
+    fallbackLng: ["en", "de", "fr", "it"],
+    debug: true
   });
 
 export default i18n;
